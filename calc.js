72861280ea0dbb21e7e7699a5028e00bfc2b6ae9
Precieux <94197472+MugishaP@users.noreply.github.com>
2021-12-27 19:23:46 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -7,7 +7,7 @@ function subtraction(num1, num2) {
 function multiplication(num1, num2) {
     return num1 * num2
 }
-function division(firstNumber, secondNumber) {
+function division(num1, num2) {
     return num1 / num2
 }
 
