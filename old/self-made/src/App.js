3b409ee09cf02a07220781078d37d970e1a8f7d1
Mugisha Precieux <precieuxmugisha@gmail.com>
2022-02-21 18:50:19 +0200

    moved

---
+++ old/self-made/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+<h1>This is Hello World in React Javascript Library</h1>
+    </div>
+  );
+}
+
+export default App;
