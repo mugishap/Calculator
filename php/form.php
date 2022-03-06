6eb6744559e4438898eac139b07fe03c4dc9a5f5
precieux33 <precieuxmugisha@gmail.com>
2022-03-06 15:01:05 +0200

    some few changes

---
+++ php/form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="<?php echo $_SERVER["PHP_SELF"] ?>" method="post">
+        <div>
+            <label for="">
+                <input type="text" name="" id="" placeholder="Enter First Name">
+            </label>
+        </div><br>
+        <div>
+            <label for="">
+                <input type="text" name="" id="" placeholder="Enter Last Name">
+            </label>
+        </div><br>
+        <div>
+            <label for="">
+                <input type="text" name="" id="" placeholder="Enter Password">
+            </label>
+        </div><br>
+        <div>
+            <label for="">
+                <input type="text" name="" id="" placeholder="Enter First Name">
+            </label>
+        </div><br>
+    </form>
+</body>
+
+</html>
